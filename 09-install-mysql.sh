9c2c920f875698218a9ad7b41b7d3d6b69bd2278
kvr-007 <venkat.apl007@gmail.com>
2025-02-12 17:21:18 -0500

    along with 09 file

---
+++ 09-install-mysql.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+  echo "ERROR:: you must have sudo access to execute this script"
+  exit 1 #other than 0
+fi
+
+dnf install mysql -y
+ if [ $? -ne 0 ]
+ then 
+   echo "Installing mysql ... FAILURE"
+   exit 1
+else 
+    echo "Installing mysql .. SUCCESS"
+ fi
+
+dnf install git -y
+if [ $? -ne 0 ]
+then
+    echo "Installing Git ... FAILURE"
+    exit 1
+else
+    echo "Installing Git ... SUCCESS"
+ fi       
