6c3609d757e48a5ceebcc1cecc87edf8f447f07d
kvr-007 <venkat.apl007@gmail.com>
2025-02-12 18:09:57 -0500

    along with 09 file

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -1,26 +1,59 @@
-#!/bin/bash
+
+ #!/bin/bash
+
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-  echo "ERROR:: you must have sudo access to execute this script"
-  exit 1 #other than 0
- fi
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1 #other than 0
+fi
 
-dnf install mysql -y
- if [ $? -ne 0 ]
- then 
-   echo "Installing mysql ... FAILURE"
-   exit 1
-else 
-    echo "Installing mysql .. SUCCESS"
- fi
+dnf list installed mysql
 
-dnf install git -y
+if [ $? -ne 0 ]
+then # not installed
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing MySQL ... FAILURE"
+        exit 1
+    else
+        echo "Installing MySQL ... SUCCESS"
+    fi
+else
+    echo "MySQL is already ... INSTALLED"
+fi
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Installing MySQL ... FAILURE"
+#     exit 1
+# else
+#     echo "Installing MySQL ... SUCCESS"
+# fi
+
+dnf list installed git
+
 if [ $? -ne 0 ]
 then
-    echo "Installing Git ... FAILURE"
-    exit 1
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing Git ... FAILURE"
+        exit 1
+    else
+        echo "Installing Git ... SUCCESS"
+    fi
 else
-    echo "Installing Git ... SUCCESS"
- fi       
+    echo "Git is already ... INSTALLED"
+fi
+
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Installing Git ... FAILURE"
+#     exit 1
+# else
+#     echo "Installing Git ... SUCCESS"
+# fi
