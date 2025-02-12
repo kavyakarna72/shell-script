b36bef2df6122bb3d27c4191fa43fa59d31d6a4b
kvr-007 <venkat.apl007@gmail.com>
2025-02-11 19:40:53 -0500

    along with 07 file

---
+++ 07-specific-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+$1, $2, $3
+All variables passed: $@
+number of variables: $#
+script name: $0
+present working directory: $PWD
+home directory of current user: $HOME
+which user is running this script: $USER
+process id of current script: $$
+process id of last command in background: $!
+
