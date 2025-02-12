c77824ad875cbe3e43be6344ceacfa723f8b16c9
kvr-007 <venkat.apl007@gmail.com>
2025-02-12 12:50:49 -0500

    along with 07 file

--- 07-specific-variables.sh
+++ 07-specific-variables.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-$1, $2, $3
-All variables passed: $@
-number of variables: $#
-script name: $0
-present working directory: $PWD
-home directory of current user: $HOME
-which user is running this script: $USER
-process id of current script: $$
-process id of last command in background: $!
+
+echo "All variables passed: $@"
+echo "number of variables: $#"
+echo "script name: $0"
+echo "present working directory: $PWD"
+echo "home directory of current user: $HOME"
+echo "which user is running this script: $USER"
+echo "process id of current script: $$"
+echo "process id of last command in background: $!"
 
